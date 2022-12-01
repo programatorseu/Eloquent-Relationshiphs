9d4049df4c5cfd983801447945679dc5d7aafed5
programators <programatorseu@gmail.com>
2022-12-01 14:16:36 +0100

    1.One-to-one

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,15 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function experience()
+    {
+        return $this->hasOne(Experience::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
